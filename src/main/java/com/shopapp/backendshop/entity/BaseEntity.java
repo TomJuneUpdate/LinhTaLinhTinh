45e14d15091cc211dc0a771be810c0527e5edca0
Tom <nhdoma02@gmail.com>
2025-02-25 21:00:43 +0700

    commit

--- src/main/java/com/shopapp/backendshop/entity/BaseEntity.java
+++ src/main/java/com/shopapp/backendshop/entity/BaseEntity.java
@@ -1,13 +1,9 @@
 package com.shopapp.backendshop.entity;
 
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Data;
-import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
